d8b24c43adbfe7fdcd6dc179a7c3fa92c332478d
Alfarabi-art <muhammadalfarabi992@gmail.com>
2025-07-18 14:16:37 +0700

    app.py

--- app.py
+++ app.py
@@ -164,13 +164,13 @@ elif selected == "📖 About":
     st.markdown("<h3 style='text-align:center;'>👥 Anggota Tim</h3>", unsafe_allow_html=True)
     col1, col2, col3 = st.columns(3)
     with col1:
-        st.markdown("<div class='feature-card'><h4>👩‍🔬 Azkia Nadira Azmi</h4><p>Front-End Developer</p></div>", unsafe_allow_html=True)
-        st.markdown("<div class='feature-card'><h4>👨‍🔬 Hanif Zaki Abizar</h4><p>Back-End Developer</p></div>", unsafe_allow_html=True)
+        st.markdown("<div class='feature-card'><h4>👩‍🔬 Azkia Nadira Azmi</h4><p>2460341</p></div>", unsafe_allow_html=True)
+        st.markdown("<div class='feature-card'><h4>👨‍🔬 Hanif Zaki Abizar</h4><p>2460384</p></div>", unsafe_allow_html=True)
     with col2:
-        st.markdown("<div class='feature-card'><h4>👩‍🔬 Muhammad Al Farabi</h4><p>UI/UX Designer</p></div>", unsafe_allow_html=True)
+        st.markdown("<div class='feature-card'><h4>👩‍🔬 Muhammad Al Farabi</h4><p>2460430</p></div>", unsafe_allow_html=True)
     with col3:
-        st.markdown("<div class='feature-card'><h4>👨‍🔬 Ovalia Kareva Betaubun</h4><p>Data Scientist</p></div>", unsafe_allow_html=True)
-        st.markdown("<div class='feature-card'><h4>👨‍🔬 Widya Aulia Putri</h4><p>QA Tester</p></div>", unsafe_allow_html=True)
+        st.markdown("<div class='feature-card'><h4>👨‍🔬 Ovalia Kareva Betaubun</h4><p>2460478</p></div>", unsafe_allow_html=True)
+        st.markdown("<div class='feature-card'><h4>👨‍🔬 Widya Aulia Putri</h4><p>2460534</p></div>", unsafe_allow_html=True)
 
 # --- Footer ---
 st.markdown("<footer>© 2025 Techmicals by Kelompok 10 | All rights reserved.</footer>", unsafe_allow_html=True)
