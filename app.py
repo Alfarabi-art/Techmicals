80fba92f689145eacbd3e9a3c27eef908f62554e
Alfarabi-art <muhammadalfarabi992@gmail.com>
2025-07-18 15:12:47 +0700

    app.py

--- app.py
+++ app.py
@@ -166,9 +166,11 @@ if selected == "🏠 Home":
     with col3:
         st.markdown("<div class='feature-card'><h3>📈 Regresi Linier</h3><p>Analisis data dan tampilkan grafik regresi.</p></div>", unsafe_allow_html=True)
 
-    if st.button("⚗ Mulai Hitung Sekarang", type="primary"):
+    if st.button("⚗ Mulai Hitung Sekarang"):
         st.session_state.show_sidebar = True
         st.session_state.menu_selected = "⚗ Reaksi Kimia"
+        st.experimental_set_query_params(menu="reaksi_kimia")  # Tambahkan query param untuk HP
+
 
     # Paksa sidebar muncul jika show_sidebar True
     if st.session_state.show_sidebar:
