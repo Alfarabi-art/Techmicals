0e4a1a9b839f97d385cf75b18212d66334df1c58
Alfarabi-art <muhammadalfarabi992@gmail.com>
2025-07-19 19:53:12 +0700

    app.py

--- app.py
+++ app.py
@@ -130,9 +130,9 @@ st.markdown("""
 import streamlit.runtime.scriptrunner as scriptrunner
 
 st.markdown('<div class="mulai-btn">', unsafe_allow_html=True)
-    if st.button("⚗ Mulai Hitung Sekarang"):
-        st.session_state.show_sidebar = True
-        st.session_state.menu_selected = "⚗ Reaksi Kimia"
+if st.button("⚗ Mulai Hitung Sekarang"):
+    st.session_state.show_sidebar = True
+    st.session_state.menu_selected = "⚗ Reaksi Kimia"
 
         # FIX: Paksa scroll ke atas & sidebar muncul
         st.components.v1.html("""
